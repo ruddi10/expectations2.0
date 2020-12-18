fe89a785eee3a2e96415b147f23a7128634411af
ruddi <rudrakshaggarwalsachin@gmail.com>
2020-12-18 23:29:07 +0530

    Add and test semantic ui

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,15 +1,35 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import { Button, Card, Image } from "semantic-ui-react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Card>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="/images/avatar/large/steve.jpg"
+        />
+        <Card.Header>Steve Sanders</Card.Header>
+        <Card.Meta>Friends of Elliot</Card.Meta>
+        <Card.Description>
+          Steve wants to add you to the group <strong>best friends</strong>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div className="ui two buttons">
+          <Button basic color="green">
+            Approve
+          </Button>
+          <Button basic color="red">
+            Decline
+          </Button>
+        </div>
+      </Card.Content>
+    </Card>
   </Layout>
 )
 
