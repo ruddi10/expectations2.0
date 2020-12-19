6e218b1a5e878722f1e289193557b8b57e9064d1
shivam8812 <shivag4321@gmail.com>
2020-12-19 14:26:57 +0530

    Add Navbar Component

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,42 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { Component } from "react"
+import { Sticky, Dropdown, Menu } from "semantic-ui-react"
+import "../styles/navbar.css"
+import { dropops } from "../data"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+export default class MenuExampleSizeHuge extends Component {
+  state = { activeItem: "Academics" }
+
+  handleItemClick = (e, { text }) => this.setState({ activeItem: text })
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <Sticky className="navbar-header">
+        <Menu
+          stackable
+          borderless
+          fluid
+          widths={7}
+          size="large"
+          className="navbar-menu"
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+          {dropops.map(dropop => (
+            <Dropdown
+              item
+              text={dropop.value}
+              className="box"
+              onClick={this.handleItemClick}
+            >
+              <Dropdown.Menu>
+                {dropop.fields.map(field => (
+                  <Dropdown.Item className="ditem">{field}</Dropdown.Item>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          ))}
+        </Menu>
+      </Sticky>
+    )
+  }
 }
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
---
+++ src/data.js
@@ -0,0 +1,57 @@
+export const dropops = [
+  {
+    value: "Academics",
+    fields: [
+      "Branches",
+      "CGPA",
+      "Branch Change",
+      "Scholarships",
+      "NPTEL",
+      "Research",
+      "First Semester",
+      "PG and Phd",
+    ],
+  },
+  {
+    value: "Co-Ciricular",
+    fields: [
+      "Cultural Groups",
+      "Technical Groups",
+      "Sports",
+      "Inter-IIT",
+      "NCC/NSS/NSO",
+    ],
+  },
+  {
+    value: "Campus",
+    fields: [
+      "Hostels",
+      "How to reach",
+      "Places",
+      "Gallery",
+      "Healthcare(Covid Measures)",
+      "Eateries",
+    ],
+  },
+  {
+    value: "Student's Corner",
+    fields: [
+      "To-do list",
+      "Packing list",
+      "IITR Lingo",
+      "In campus commute and payments",
+      "FAQs",
+    ],
+  },
+  { value: "Experiences", fields: ["Freshman", "Alumni"] },
+  { value: "Placements", fields: ["Analysis"] },
+  {
+    value: "Parent's Section",
+    fields: [
+      "Anti-Ragging policy",
+      "Banking, Fees and loans",
+      "Where to stay",
+      "Secqurity",
+    ],
+  },
+]
