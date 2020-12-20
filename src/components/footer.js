95e188a869370ea5cf7088acc2274d454aeca805
ruddi <rudrakshaggarwalsachin@gmail.com>
2020-12-21 00:36:26 +0530

    Modify footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import { Button, Icon } from "semantic-ui-react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import { Button, Icon, Menu } from "semantic-ui-react"
 import Image from "gatsby-image"
 import "../styles/footer.css"
 const query = graphql`
@@ -29,8 +29,35 @@ export default function Footer() {
           </Button>
         </div>
       </div>
-      <div className="middle"></div>
-      <div className="bottom"></div>
+      <div className="middle text">
+        <div>Explore</div>
+        <Menu.Item as={Link}>Student's Corner</Menu.Item>
+        <Menu.Item as={Link}>Academics</Menu.Item>
+        <Menu.Item as={Link}>Placement</Menu.Item>
+        <Menu.Item as={Link}>Co-curricular</Menu.Item>
+        <Menu.Item as={Link}>Campus</Menu.Item>
+        <Menu.Item as={Link}>Parent's Corner</Menu.Item>
+        <Menu.Item as={Link}>Experiences</Menu.Item>
+      </div>
+      <div className="bottom text">
+        <div className="grid-header headings">Quick Links</div>
+        <div className="grid-header headings">Other Resources</div>
+        <div className="grid-header headings">Connect With Us</div>
+        <Menu.Item as={Link}>Student's Corner</Menu.Item>
+        <div>1</div>
+        <div>1</div>
+        <Menu.Item as={Link}>How to get to Roorkee?</Menu.Item>
+        <div>1</div>
+        <Menu.Item as={Link}>Gallery</Menu.Item>
+        <div>1</div>
+        <div>1</div>
+        <Menu.Item as={Link}>Know Your Branch</Menu.Item>
+        <div>1</div>
+        <div>1</div>
+        <Menu.Item as={Link}>FAQs</Menu.Item>
+        <div>1</div>
+        <div>1</div>
+      </div>
     </div>
   )
 }
