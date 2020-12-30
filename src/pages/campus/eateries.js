bfa7231b4bc5e97ea430c9bfc43722b3b08e591d
shivam8812 <shivag4321@gmail.com>
2020-12-31 00:04:15 +0530

    Update eateries and card

--- src/pages/campus/eateries.js
+++ src/pages/campus/eateries.js
@@ -4,13 +4,95 @@ import Image from "gatsby-image"
 import { eateries } from "../../data"
 import Layout from "../../components/layout"
 import { graphql } from "gatsby"
+import "../../styles/eateries.css"
+import { Component } from "react"
+import ContentWithImage from "../../components/contentWithImage"
 
-const Eateries = props => {
-  return (
-    <div>
-      <Layout>
-        <h2 className="dark-color-head">Eateries</h2>
-        <Image
+class Eateries extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { type: "veg" }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick = e => {
+    this.setState({
+      type: e.target.value,
+    })
+    console.log(this.state.type)
+  }
+  render(props) {
+    const ueateries = eateries.filter(eat => eat.type === this.state.type)
+    console.log(this.props.data)
+    return (
+      <div>
+        <Layout>
+          <h2 className="dark-color-head">Eateries</h2>
+          <div className="eat-nav">
+            <button
+              className="eat-button text"
+              value={"veg"}
+              onClick={this.handleClick}
+            >
+              Pure Veg
+            </button>
+            <button
+              className="eat-button text"
+              value={"nonveg"}
+              onClick={this.handleClick}
+            >
+              Non Veg
+            </button>
+            <button
+              className="eat-button text"
+              value={"dessert"}
+              onClick={this.handleClick}
+            >
+              Dessert
+            </button>
+            <hr className="hr" />
+          </div>
+          {ueateries.map((ueat, index) => (
+            <div className="eat-div">
+              <ContentWithImage
+                {...ueat}
+                fluid={
+                  this.props.data.allFile.edges.find(
+                    edge => edge.node.name === ueat.display
+                  ).node.childImageSharp.fluid
+                }
+                r={index % 2}
+              />
+            </div>
+          ))}
+        </Layout>
+      </div>
+    )
+  }
+}
+
+export default Eateries
+
+export const query = graphql`
+  {
+    allFile(filter: { relativeDirectory: { eq: "eateries" } }) {
+      totalCount
+      edges {
+        node {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+          name
+        }
+      }
+    }
+  }
+`
+
+{
+  /* <Image
           className="expanded-image"
           fluid={props.data.file.childImageSharp.fluid}
         />
@@ -26,22 +108,5 @@ const Eateries = props => {
               />
             </div>
           ))}
-        </div>
-      </Layout>
-    </div>
-  )
+        </div> */
 }
-
-export default Eateries
-
-export const query = graphql`
-  {
-    file(relativePath: { eq: "eateriesback.jpg" }) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid
-        }
-      }
-    }
-  }
-`
--- src/pages/co-curricular/inter-iit.js
+++ src/pages/co-curricular/inter-iit.js
@@ -2,6 +2,7 @@ import React from "react"
 import ContentWithImage from "../../components/contentWithImage"
 import Layout from "../../components/layout"
 import { interIITs } from "../../data"
+import { graphql } from "gatsby"
 function InterIit(props) {
   return (
     <div>
@@ -24,7 +25,7 @@ export default InterIit
 
 export const query = graphql`
   {
-    file(relativePath: { eq: "eateriesback.jpg" }) {
+    file(relativePath: { eq: "tin_tin1.jpg" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid
